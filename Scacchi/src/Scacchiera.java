50e7d8f5827fe396192312f73499475902a74349
manueldallavalle <manuel.dallavalle0@gmail.com>
2015-03-17 11:45:12 +0100

    Aggiunta menu (JMenuBar)
    * Aggiunta del menu con opzioni (Nuova Partita e EscI)

--- Scacchi/src/Scacchiera.java
+++ Scacchi/src/Scacchiera.java
@@ -1,10 +1,35 @@
+import java.awt.event.KeyEvent;
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 
 public class Scacchiera extends JFrame {
     private static final long serialVersionUID = 1L; 
     private DisegnaScacchiera scacc;
+    private JMenuBar menuBar;
+    private JMenu menu;
+    private JMenuItem nuovaPartita;
+    private JMenuItem esciPartita;
     
     public Scacchiera(){
+        menuBar = new JMenuBar();
+        menu = new JMenu("Menu");
+        menu.setMnemonic(KeyEvent.VK_M); // HOTKEY M
+        menuBar.add(menu);
+        
+        nuovaPartita = new JMenuItem("Nuova Partita", KeyEvent.VK_N);  // HOTKEY N
+        esciPartita = new JMenuItem("Esci", KeyEvent.VK_E);  // HOTKEY E
+        menu.add(nuovaPartita);
+        menu.add(esciPartita);
+        
+        nuovaPartita.addActionListener(new GestoreAzione());
+        nuovaPartita.setActionCommand("nuovaPartita");
+        esciPartita.addActionListener(new GestoreAzione());
+        esciPartita.setActionCommand("esciPartita");
+
+        this.setJMenuBar(menuBar);
+        
         scacc = new DisegnaScacchiera();
         add(scacc);
         pack();
